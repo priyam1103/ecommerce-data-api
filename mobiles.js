63fa6617833ee86a451df130b207e5adfc40c483
priyam1103 <priyampoddar89@gmail.com>
2021-03-21 02:24:19 +0530

    updated data

--- mobiles.js
+++ mobiles.js
@@ -308,7 +308,7 @@ module.exports = {
     },
   ],
   pids: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
-  price: ["Under ₹1000", "₹5000 - ₹10000", "₹10000 - ₹20000", "Over ₹20000"],
+  price: ["₹5000 - ₹10000", "₹10000 - ₹20000", "Over ₹20000"],
   brands: ["Apple", "Redmi", "Samsung"],
   internal_memory: ["8 GB", "32 GB", "64 GB", "128 GB"],
   ram: ["8GB & above", "6GB", "4GB", "3GB", "2GB"],
